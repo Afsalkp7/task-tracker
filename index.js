0eb0e9bfffa2acdc114450de58c7b737a7839117
Afsalkp7 <afsalkpmanu31@gmail.com>
2024-03-08 10:23:40 +0530

    cors completed

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const app = express()
 const bodyParser = require("body-parser");
+const cors = require('cors');
 const PORT = 3010
 const mongoose = require("./database/connect");
 const userRoutes = require ("./routes/user")
 app.use(bodyParser.json());
-
+app.use(cors())
 app.use('/user', userRoutes);
 
 
