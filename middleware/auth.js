c47a73daaec06d48f108588c6e99b9bf8ce15c22
Afsalkp7 <afsalkpmanu31@gmail.com>
2024-03-08 14:55:05 +0530

    task added

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,6 +4,7 @@ const secretKey = "authtokenfortaskmanager";
 
 function auth(req, res, next) {
     const token = req.headers.authorization;
+    console.log(token);
     if (!token) {
         return res.status(401).json({ message: 'Unauthorized' });
     } else {
